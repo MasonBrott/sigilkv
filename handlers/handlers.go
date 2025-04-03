ca77e3e8bf9f176d18b6b2b5e03e3fa42964e6fe
masonbrott <masonbrott@gmail.com>
2025-04-02 23:02:32 -0500

    implemented fasthttp handlers

---
+++ handlers/handlers.go
@@ -0,0 +1,46 @@
+package handlers
+
+import (
+	"errors"
+
+	"github.com/masonbrott/sigilkv/core"
+	"github.com/valyala/fasthttp"
+)
+
+func KVPutHandler(ctx *fasthttp.RequestCtx) {
+	key := ctx.UserValue("key").(string)
+	value := string(ctx.PostBody())
+
+	err := core.Put(key, string(value))
+	if err != nil {
+		ctx.Error(err.Error(), 500)
+		return
+	}
+
+	ctx.SetStatusCode(201)
+}
+
+func KVGetHandler(ctx *fasthttp.RequestCtx) {
+	key := ctx.UserValue("key").(string)
+
+	value, err := core.Get(key)
+	if errors.Is(err, core.ErrorNoSuchKey) {
+		ctx.Error(err.Error(), 404)
+		return
+	}
+
+	if err != nil {
+		ctx.Error(err.Error(), 500)
+		return
+	}
+
+	ctx.Write([]byte(value))
+}
+
+func KVDeleteHandler(ctx *fasthttp.RequestCtx) {
+	key := ctx.UserValue("key").(string)
+
+	core.Delete(key)
+
+	ctx.SetStatusCode(200)
+}
--- main.go
+++ main.go
@@ -4,14 +4,15 @@ import (
 	"log"
 
 	"github.com/fasthttp/router"
+	"github.com/masonbrott/sigilkv/handlers"
 	"github.com/valyala/fasthttp"
 )
 
 func main() {
 	r := router.New()
-	r.PUT("/v1/key/{key}", kvputhandler)
-	r.GET("/v1/key/{key}", kvgethandler)
-	r.DELETE("/v1/key/{key}", kvdeletehandler)
+	r.PUT("/v1/key/{key}", handlers.KVPutHandler)
+	r.GET("/v1/key/{key}", handlers.KVGetHandler)
+	r.DELETE("/v1/key/{key}", handlers.KVDeleteHandler)
 
 	log.Fatal(fasthttp.ListenAndServe(":8080", r.Handler))
 }
